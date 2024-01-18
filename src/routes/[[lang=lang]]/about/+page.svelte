4e545196aedf331c95447e44661d61e4890ced01
Loris Sigrist <sigrist@hasch.io>
2024-01-18 11:59:13 +0100

    Initial commit

---
+++ src/routes/[[lang=lang]]/about/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import en from "./en.svelte";
+	import de from "./de.svelte";
+	import * as m from "$paraglide/messages.js";
+  	import { languageTag } from "$paraglide/runtime";
+
+	const lang = {
+		en,
+		de,
+	};
+</script>
+
+<svelte:head>
+  <title>{m.about()}</title>
+  <meta name="description" content={m.about_this_app()} />
+</svelte:head>
+
+<svelte:component this={lang[languageTag()]} />
