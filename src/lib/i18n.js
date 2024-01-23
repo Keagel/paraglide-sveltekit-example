3e4899ca999315ea2bbdc6a237f9c6ac289455a3
Loris Sigrist <sigrist@hasch.io>
2024-01-23 14:31:17 +0100

    bump version & add translated path

--- src/lib/i18n.js
+++ src/lib/i18n.js
@@ -1,4 +1,11 @@
-import { createI18n } from "@inlang/paraglide-js-adapter-sveltekit"
-import * as runtime from "$paraglide/runtime"
+import { createI18n } from "@inlang/paraglide-js-adapter-sveltekit";
+import * as runtime from "$paraglide/runtime";
 
-export const i18n = createI18n(runtime);
+export const i18n = createI18n(runtime, {
+  pathnames: {
+    "/about": {
+      en: "/about",
+      de: "/ueber",
+    },
+  },
+});
