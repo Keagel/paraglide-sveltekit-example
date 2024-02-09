026ce9816c094e50e6c31ec08500caceb5a2fe49
Loris Sigrist <sigrist@hasch.io>
2024-02-09 15:55:06 +0100

    Bump dependencies

--- src/lib/i18n.js
+++ src/lib/i18n.js
@@ -1,13 +1,19 @@
 import { createI18n } from "@inlang/paraglide-js-adapter-sveltekit";
 import * as runtime from "$paraglide/runtime";
+import * as m from "$paraglide/messages";
 
 export const i18n = createI18n(runtime, {
   pathnames: {
-    "/about": {
-      en: "/about",
-      de: "/ueber",
-    },
+    //You can use message-functions
+    "/about": m.about_path,
+
+    //Or a simple object
+    "/sverdle": {
+      en: "/sverdle",
+      de: "/sverdle"
+    }
   },
+
   textDirection: {
     en: "ltr",
     de: "ltr"
